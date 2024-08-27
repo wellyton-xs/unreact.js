b18b8995b6a74110d4fd1fec1a739005c24d44ad
wellyton-xs <wellyton.offcer@gmail.com>
2024-08-27 15:06:48 -0300

    add attributes for elements

--- unreact.js
+++ unreact.js
@@ -1,20 +1,42 @@
-export function createElement(tag, child = "", atributes = ""){
+function debug(desc = "", arg){
+    console.log(desc)
+    console.log("typeof:", typeof(arg))
+    console.log("vof output", arg)
+}
+
+export function createElement(tag, child = "", attributes = {}){
     let newElement = document.createElement(tag)
-    let content = document.createTextNode(child)
+    let content = document.createTextNode(child);
+    
+    if (attributes) {
+        Object.keys(attributes).forEach(attr => {
+            newElement.setAttribute(attr, attributes[attr]);
+        });
+    }
+
     newElement.appendChild(content)
     return newElement
 };
+
 export function printElement(tag){
     let element = createElement(tag);
     console.log(element);
 }
+
 export function updateDOM(element){
     const root = document.getElementById("#root")
     document.body.insertBefore(element, root)
 }
 
-export function h1(child){
-    let h1 = createElement("h1", child)
+export function div(child){
+    let div = createElement("div", child)
+    let element = updateDOM(div)
+
+    return element
+}
+
+export function h1(child, attr){
+    let h1 = createElement("h1", child, attr)    
     let element = updateDOM(h1)
     
     return element
