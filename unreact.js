eb755cf222f07f82cf4562c3999d885fcd0e8176
wellyton-xs <wellyton.offcer@gmail.com>
2024-09-03 08:46:20 -0300

    add new tags to unreact

--- unreact.js
+++ unreact.js
@@ -6,13 +6,6 @@ export function div(...children){
     return element
 }
 
-export function h1(...children){
-    let element = createElement("h1", ...children)
-    updateDOM(element)
-
-    return element
-}
-
 export function p(...children){
     let element = createElement("p", ...children)
     updateDOM(element)
@@ -31,5 +24,131 @@ export function input(...children){
     let element = createElement("input", ...children)
     updateDOM(element)
     
+    return element
+}
+
+export function ul(...children){
+    let element = createElement("ul", ...children)
+    updateDOM(element)
+    
+    return element
+}
+
+export function li(...children){
+    let element = createElement("li", ...children)
+    updateDOM(element)
+    
+    return element
+}
+
+export function span(...children){
+    let element = createElement("span", ...children)
+    updateDOM(element)
+    
+    return element
+}
+
+export function strong(...children){
+    let element = createElement("strong", ...children)
+    updateDOM(element)
+    
+    return element
+}
+
+export function img(...children){
+    let element = createElement("img", ...children)
+    updateDOM(element)
+    
+    return element
+}
+
+export function textarea(...children){
+    let element = createElement("textarea", ...children)
+    updateDOM(element)
+    
+    return element
+}
+
+export function br(...children){
+    let element = createElement("br", ...children)
+    updateDOM(element)
+    
+    return element
+}
+
+export function tr(...children){
+    let element = createElement("tr", ...children)
+    updateDOM(element)
+    
+    return element
+}
+
+export function main(...children){
+    let element = createElement("main", ...children)
+    updateDOM(element)
+    
+    return element
+}
+
+export function header(...children){
+    let element = createElement("header", ...children)
+    updateDOM(element)
+    
+    return element
+}
+
+export function section(...children){
+    let element = createElement("section", ...children)
+    updateDOM(element)
+    
+    return element
+}
+
+export function article(...children){
+    let element = createElement("input", ...children)
+    updateDOM(element)
+    
+    return element
+}
+
+export function footer(...children){
+    let element = createElement("footer", ...children)
+    updateDOM(element)
+    
+    return element
+}
+
+export function h1(...children){
+    let element = createElement("h1", ...children)
+    updateDOM(element)
+
+    return element
+}
+
+export function h2(...children){
+    let element = createElement("h2", ...children)
+    updateDOM(element)
+
+    return element
+}
+
+export function h3(...children){
+    let element = createElement("h3", ...children)
+    updateDOM(element)
+
+    return element
+}
+
+export function h4(...children){
+    let element = createElement("h4", ...children)
+    updateDOM(element)
+
+    return element
+}
+
+export function h5(...children){
+    let element = createElement("h5", ...children)
+    updateDOM(element)
+
     return element
 }
